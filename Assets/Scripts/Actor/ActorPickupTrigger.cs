60c94f5c5d0905247ca44a72caf1c324c6193e2b
Joseph Quested <josephquested@gmail.com>
2017-04-27 13:42:28 +1200

    update default unity c# template

---
+++ Assets/Scripts/Actor/ActorPickupTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorPickupTrigger : MonoBehaviour {
+
+	// SYSTEM //
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Item/Item.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType { Weapon };
+
+public class Item : MonoBehaviour {
+
+	// SYSTEM //
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+
+	}
+
+	// ITEM //
+
+	public ItemType itemType;
+
+	public bool inInventory;
+
+	public void Pickup ()
+	{
+
+	}
+
+	public void Throw ()
+	{
+
+	}
+}
